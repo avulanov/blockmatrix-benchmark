d7c6a84f93c3ff2dfa854efd34a9b884f7078183
Alexander Ulanov <nashb@yandex.ru>
2015-07-13 17:11:57 -0700

    Force native blas: need to cache the matrices

--- src/blockmatrix.scala
+++ src/blockmatrix.scala
@@ -1,10 +1,10 @@
 import org.apache.spark.mllib.linalg.Matrices
 import org.apache.spark.mllib.linalg.distributed.BlockMatrix
 val parallelism = 5
-val rows = 100000
-val columns = 10000
+val rows = 1000
+val columns = 100
 val size = rows * columns
-val blockSize = 10000
+val blockSize = 100
 assert(rows % blockSize == 0)
 assert(columns % blockSize == 0)
 val rowBlocks = rows / blockSize
@@ -14,7 +14,10 @@ val rdd = sc.parallelize( {
 	}, parallelism).map( coord => (coord, Matrices.rand(blockSize, blockSize, util.Random.self)))
 val bm = new BlockMatrix(rdd, blockSize, blockSize).cache()
 bm.validate()
+val mb = bm.transpose.cache()
+mb.validate()
 val t = System.nanoTime()
-val ata = bm.transpose.multiply(bm)
+val ata = bm.multiply(mb)
 ata.validate()
 println(rows + "x" + columns + ", block:" + blockSize + "\t" + (System.nanoTime() - t) / 1e9) 
+
